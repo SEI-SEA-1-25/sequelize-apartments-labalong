38aa25644638570a0b179b4dfe93846f6a05134c
weston-bailey <wdbailey87@gmail.com>
2021-02-18 16:55:00 -0800

    1:M

--- index.js
+++ index.js
@@ -13,11 +13,49 @@ sequelize model:generate --name=property --attributes ownerId:integer,name:text,
 async function creating() {
   try {
     // simple create 
-    const owner = await db.owner.create({
-      name: 'Jasmin',
-      age: 21
+    // const owner = await db.owner.create({
+    //   name: 'Jasmin',
+    //   age: 21
+    // })
+
+    // search options
+    const options = {
+      where: { name: 'Jane' }, 
+      defualts: { age: 35 }
+    }
+
+    // array destructuring syntax
+    const [owner, created] = await db.owner.findOrCreate(options)
+    if(created === true) console.log(`${owner.name} was created!`)
+
+    const propertyOptions = {
+      where: {  name: 'Mesa Verde' },
+      defaults: { units: 1000 }
+    }
+    // array destructuring is variables
+    const [property, whatever] = await db.property.findOrCreate(propertyOptions)
+    if(whatever === true) console.log(`${property.name} was created!`)
+
+    await owner.addProperty(property)
+
+    const foundProperties = await owner.getProperties()
+    // console.log(foundProperties)
+
+    // new owner with properties included
+    const newOwner = {
+      name: 'Jack',
+      age: 70,
+    }
+
+    // create out owner
+    const createdOwner = await db.owner.create(newOwner)
+
+    await createdOwner.createProperty({
+      name: 'New Apartments',
+      units: 45
     })
-    console.log(owner.name)
+
+
   } catch (error) {
     console.log(error)
   }
@@ -28,16 +66,24 @@ async function creating() {
 // READ
 async function reading() {
   try {
-    const owners = await db.owner.findAll()
+    // const owners = await db.owner.findAll()
+    // owners.forEach(owner => {
+    //   console.log(`${owner.name} is ${owner.age} years old`)
+    // })
+    // const william = await db.owner.findOne({
+    //   where: {
+    //     id: 2
+    //   }
+    // })
+    // console.log(`found user ${william.name} with and id of ${william.id}`)
+    const owners = await db.owner.findAll({ include: db.property })
     owners.forEach(owner => {
-      console.log(`${owner.name} is ${owner.age} years old`)
+      owner.properties.forEach(property => {
+        console.log(property)
+        // console.log(`${owner.name} owns ${property.name}`)
+      })
     })
-    const william = await db.owner.findOne({
-      where: {
-        id: 2
-      }
-    })
-    console.log(`found user ${william.name} with and id of ${william.id}`)
+    // console.log(owners)
   } catch(error) {
     console.log(error)
   }
@@ -76,4 +122,4 @@ async function destroying() {
   }
 }
 
-destroying()
+// destroying()
